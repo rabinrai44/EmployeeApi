6a0b8fb5093cf1617e28f31af8e72e44a85eb29b
Rabin Rai <rabinrai44@gmail.com>
2019-11-14 16:19:20 -0500

    Added Models, Data and configure Database.

---
+++ EmployeeApi/Data/EmployeeApiContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Data
+{
+    public class EmployeeApiContext: DbContext
+    {
+        public EmployeeApiContext(DbContextOptions<EmployeeApiContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeApi/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public byte Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+    }
+}
