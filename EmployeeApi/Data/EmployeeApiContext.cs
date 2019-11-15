76116b309222e060c4b1e19ded8093f803e8fac4
Rabin Rai <rabinrai44@gmail.com>
2019-11-15 00:53:35 -0500

    Populate Initial Employee data.

--- EmployeeApi/Data/EmployeeApiContext.cs
+++ EmployeeApi/Data/EmployeeApiContext.cs
@@ -11,5 +11,29 @@ namespace EmployeeApi.Data
         }
 
         public DbSet<Employee> Employees { get; set; }
+
+        // Seed Initial Employee Data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Mark",
+                    LastName = "Kall",
+                    Position = "Developer",
+                    Salary = 80000.0
+                },
+
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Jason",
+                    LastName = "Rai",
+                    Position = "Assistant",
+                    Salary = 20000.0
+                }
+            );
+        }
     }
 }
