6cedeff71a36407e07bdd83f784a55e930c7cf2c
Rabin Rai <rabinrai44@gmail.com>
2019-11-15 00:13:24 -0500

    Changed Employee Id type byte to int. Having trouble set identity with byte in EF.

--- EmployeeApi/Models/Employee.cs
+++ EmployeeApi/Models/Employee.cs
@@ -5,7 +5,7 @@ namespace EmployeeApi.Models
     public class Employee
     {
         [Key]
-        public byte Id { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
